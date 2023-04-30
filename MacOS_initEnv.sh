fdf0281ed82084731e787fc58a1f1d5744a9d2c2
dgmadmin <dgmadmin@dgmadmins-MacBook-Pro.local>
2023-04-30 19:00:39 +0200

    gg

--- MacOS_initEnv.sh
+++ MacOS_initEnv.sh
@@ -3,4 +3,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap ./Brewfile_pkg.list
 wget https://github.com/3t0n/LayoutSwitcher/releases/download/v.1.3.0/LayoutSwitcher.dmg
+brew install python ansible
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+brew install iterm2
+brew tap homebrew/cask-fonts     
+brew install font-anonymice-nerd-font
+echo eval "$(oh-my-posh init zsh)" >> ~/.zshrc
+
+brew leaast > brew.txt
 
