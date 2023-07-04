61a0d8108151f28bc9c32180cf8f0411ca62fce6
merlottt <dimerlot@gmail.com>
2023-07-04 21:03:16 +0200

    Update MacOS_initEnv.sh

--- MacOS_initEnv.sh
+++ MacOS_initEnv.sh
@@ -2,7 +2,11 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap ./Brewfile_pkg.list
+brew install wget
 wget https://github.com/3t0n/LayoutSwitcher/releases/download/v.1.3.0/LayoutSwitcher.dmg
+hdiutil mount LayoutSwitcher.dmg
+sudo cp -R "/Volumes/LayoutSwitcher/LayoutSwitcher.app" /Applications 
+hdiutil unmount /Volumes/LayoutSwitcher 
 brew install python ansible
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 brew install iterm2
